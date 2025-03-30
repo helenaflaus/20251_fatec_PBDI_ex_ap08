0d6abfced539af58a35e22f3582b3fe6c9791ab0
Helena Flausino <helenaflausino@gmail.com>
2025-03-30 19:54:43 -0300

    chore(ap08): implementa exercicio 1.1

---
+++ script.sql
@@ -0,0 +1,13 @@
+--Para os exercícios que não especifiquem intervalos explicitamente, considere os seguintes
+--intervalos.
+--Números inteiros: [1, 100]
+--Números reais: [1, 10]
+
+--1.1 Faça um programa que gere um valor inteiro e o exiba.
+DO $$
+DECLARE
+    n1 INT;
+    limite_inferio :=
+BEGIN
+
+END $$;
